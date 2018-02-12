563b562880c15849e6859288ee4d5aa90252acb9
Jacob Fricke <jacob.fricke@sense-it.de>
2018-02-12 09:55:44 +0100

    use 'isJSON' directly

--- test/isJSON.js
+++ test/isJSON.js
@@ -1,41 +1,41 @@
-const mergeJSON = require('./../index.js') ;
+const isJSON = require('./../index.js').isJSON ;
 
 const expect = require('chai').expect ;
 
 
 describe('isJSON', function(){
 	it('should return \'false\' on any object but JSON', function(){
-		expect(mergeJSON.isJSON(new Date()), "fail on date object 'new Date()'").to.be.false ;
-		expect(mergeJSON.isJSON(/.*/gi), "fail on regex '/.*/gi'").to.be.false ;
+		expect(isJSON(new Date()), "fail on date object 'new Date()'").to.be.false ;
+		expect(isJSON(/.*/gi), "fail on regex '/.*/gi'").to.be.false ;
 	}) ;
 
 	it('should return \'false\' on strings', function(){
-		expect(mergeJSON.isJSON("test"), "fail on string 'test'").to.be.false ;
-		expect(mergeJSON.isJSON(""), "fail on empty string").to.be.false ;
+		expect(isJSON("test"), "fail on string 'test'").to.be.false ;
+		expect(isJSON(""), "fail on empty string").to.be.false ;
 	}) ;
 
 	it('should return \'false\' on numbers', function(){
-		expect(mergeJSON.isJSON(-1), "fail on negative number (-1)").to.be.false ;
-		expect(mergeJSON.isJSON(1), "fail on positive number (1)").to.be.false ;
-		expect(mergeJSON.isJSON(0), "fail on zero (0)").to.be.false ;
-		expect(mergeJSON.isJSON(NaN), "fail on 'NaN'").to.be.false ;
+		expect(isJSON(-1), "fail on negative number (-1)").to.be.false ;
+		expect(isJSON(1), "fail on positive number (1)").to.be.false ;
+		expect(isJSON(0), "fail on zero (0)").to.be.false ;
+		expect(isJSON(NaN), "fail on 'NaN'").to.be.false ;
 	}) ;
 
 	it('should return \'false\' on boolean', function(){
-		expect(mergeJSON.isJSON(true), "fail on 'true'").to.be.false ;
-		expect(mergeJSON.isJSON(false), "fail on 'false'").to.be.false ;
+		expect(isJSON(true), "fail on 'true'").to.be.false ;
+		expect(isJSON(false), "fail on 'false'").to.be.false ;
 	}) ;
 
 	it('should return \'false\' on \'null\' or \'undefined\'', function(){
-		expect(mergeJSON.isJSON(null), "fail on 'null'").to.be.false ;
-		expect(mergeJSON.isJSON(undefined), "fail on 'undefined'").to.be.false ;
+		expect(isJSON(null), "fail on 'null'").to.be.false ;
+		expect(isJSON(undefined), "fail on 'undefined'").to.be.false ;
 	}) ;
 
 	it('should return \'false\' on arrays', function(){
-		expect(mergeJSON.isJSON([]), "fail on '[]'").to.be.false ;
+		expect(isJSON([]), "fail on '[]'").to.be.false ;
 	}) ;
 
 	it('should return \'true\' on JSON objects', function(){
-		expect(mergeJSON.isJSON({}), "fail on '{}'").to.be.true ;
+		expect(isJSON({}), "fail on '{}'").to.be.true ;
 	}) ;
 }) ;
